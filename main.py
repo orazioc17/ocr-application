72a1d6e3daa1bf87d1cbc8ee747efbc95994aceb
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2023-01-03 07:06:25 -0400

    OCR Application endpoint

---
+++ main.py
@@ -0,0 +1,16 @@
+import shutil
+
+from fastapi import FastAPI, File, UploadFile
+
+import pytesseract
+
+
+app = FastAPI()
+
+
+@app.post('/ocr')
+def ocr(image: UploadFile = File(...)):
+    file_path = 'txtFile'
+    with open(file_path, 'w+b') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return pytesseract.image_to_string(file_path, lang='eng')
